435744ad1d0051ce702decb046c1022468c0df76
Patryk Biernat <bearnacki@gmail.com>
2019-07-13 14:04:20 +0200

    readme update

--- src/App.js
+++ src/App.js
@@ -61,10 +61,8 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('http://localhost:3000')
-    .then(response => {
-      response.json()
-    })
+    fetch('http://localhost:3000/')
+    .then(response => response.json())
     .then(console.log);
   }
 
